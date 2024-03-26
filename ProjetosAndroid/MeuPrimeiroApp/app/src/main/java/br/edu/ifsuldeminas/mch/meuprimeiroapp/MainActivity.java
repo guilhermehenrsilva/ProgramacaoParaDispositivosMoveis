e0c2c64c4b9b3e3b368ae3c5dc149740ad1c6193
Guilherme Henrique de Souza Silva <guilherme_he_nrrique@hotmail.com>
2024-03-26 10:50:22 -0300

    Atualizado 26/03/2024

--- ProjetosAndroid/MeuPrimeiroApp/app/src/main/java/br/edu/ifsuldeminas/mch/meuprimeiroapp/MainActivity.java
+++ ProjetosAndroid/MeuPrimeiroApp/app/src/main/java/br/edu/ifsuldeminas/mch/meuprimeiroapp/MainActivity.java
@@ -3,6 +3,9 @@ package br.edu.ifsuldeminas.mch.meuprimeiroapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //acessar o botão
+        Button buttonLogin = findViewById(R.id.buttonLogin);
+
+        //acessar o onClickListener
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(view.getContext(),
+                        R.string.login_succefull,Toast.LENGTH_LONG);
+
+                toast.show();
+            }
+        });
+
+        //passar uma View.onclickListener
+        //Implementar uma subclasse  de View.onClickListener
+
     }
 }
