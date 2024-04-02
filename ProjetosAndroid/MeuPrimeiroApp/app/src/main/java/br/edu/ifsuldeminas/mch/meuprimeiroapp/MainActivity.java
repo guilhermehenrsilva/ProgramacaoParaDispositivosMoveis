0f53329f4f468c171edc0d7e8a01bddc8c5051c5
Guilherme Henrique de Souza Silva <guilherme_he_nrrique@hotmail.com>
2024-04-02 08:11:04 -0300

    Atualizado 02/04/2024

--- ProjetosAndroid/MeuPrimeiroApp/app/src/main/java/br/edu/ifsuldeminas/mch/meuprimeiroapp/MainActivity.java
+++ ProjetosAndroid/MeuPrimeiroApp/app/src/main/java/br/edu/ifsuldeminas/mch/meuprimeiroapp/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,20 +17,37 @@ public class MainActivity extends AppCompatActivity {
 
         //acessar o botão
         Button buttonLogin = findViewById(R.id.buttonLogin);
+        EditText userName = findViewById(R.id.editTextUser);
+        EditText userPW = findViewById(R.id.editTextNumberPassword);
 
-        //acessar o onClickListener
-        buttonLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast toast = Toast.makeText(view.getContext(),
-                        R.string.login_succefull,Toast.LENGTH_LONG);
+        // recuperando login e senha como string
+       String userNameStr = userName.getText().toString();
+       String userPWStr = userPW.getText().toString();
 
-                toast.show();
+
+        buttonLogin.setOnClickListener(view -> {
+           if (userNameStr.equals("")){
+               Toast toast = Toast.makeText(view.getContext(),R.string.login_user_name_empty, Toast.LENGTH_SHORT);
+
+               toast.show();
+               return;
             }
+
+           if (userPWStr.equals("")){
+               Toast toast = Toast.makeText(view.getContext(),R.string.login_user_name_empty, Toast.LENGTH_SHORT);
+
+               toast.show();
+               return;
+           }
         });
+
 
         //passar uma View.onclickListener
         //Implementar uma subclasse  de View.onClickListener
 
+    }
+    public void forgotPW (View view)  {
+        Toast toast = Toast.makeText(view.getContext(), R.string.login_user_forgotPw_message,Toast.LENGTH_LONG);
+        toast.show();
     }
 }
