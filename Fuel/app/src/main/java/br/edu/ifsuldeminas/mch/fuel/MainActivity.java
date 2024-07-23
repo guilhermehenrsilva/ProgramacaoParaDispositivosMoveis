7ad2eb0c42e48abdfae47ff873d08666421c472e
Guilherme Henrique <guilherme_he_nrrique@hotmail.com>
2024-07-23 09:52:49 -0300

    Atualizado 23/07/2024

--- Fuel/app/src/main/java/br/edu/ifsuldeminas/mch/fuel/MainActivity.java
+++ Fuel/app/src/main/java/br/edu/ifsuldeminas/mch/fuel/MainActivity.java
@@ -1,10 +1,14 @@
 package br.edu.ifsuldeminas.mch.fuel;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -14,23 +18,13 @@ import com.google.android.material.textfield.TextInputEditText;
 public class MainActivity extends AppCompatActivity {
 
     private TextInputEditText textInputEditTextEtanol;
-
     private TextInputEditText textInputEditTextGas;
-
-    private Button buttonCalcular;
-
+    private Button btnCalcular;
     private ImageView imageViewResult;
-
     private ImageView imageViewShare;
-
     private TextView textViewResult;
-
+    private String tip;
     private Double etanolPrice, gasPrice;
-
-    private String tip ;
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,46 +33,84 @@ public class MainActivity extends AppCompatActivity {
 
         textInputEditTextEtanol = findViewById(R.id.textInputEditTextEtanol);
         textInputEditTextGas = findViewById(R.id.textInputEditTextGas);
-       buttonCalcular = findViewById(R.id.buttonCalcular);
+        btnCalcular = findViewById(R.id.buttonCalcular);
         imageViewResult = findViewById(R.id.imageViewFuel);
         imageViewShare = findViewById(R.id.imageViewShare);
         textViewResult = findViewById(R.id.textViewMessage);
 
-        buttonCalcular.setOnClickListener(View -> {
+        btnCalcular.setOnClickListener(view -> {
             String etanolPriceStr = textInputEditTextEtanol.getText().toString();
-            String gasPriceStr = textInputEditTextGas.getText().toString();
+            String gasPrceStr = textInputEditTextGas.getText().toString();
 
-            if (etanolPriceStr.equals("")){
-                Toast.makeText(getApplicationContext(),"O valor do Etanol não foi informado!", Toast.LENGTH_LONG).show();
+            if(etanolPriceStr.equals("")){
+                Toast.makeText(getApplicationContext(),"O valor do etanol não foi informado",
+                        Toast.LENGTH_LONG).show();
                 return;
             }
 
-            if (gasPriceStr.equals("")){
-                Toast.makeText(getApplicationContext(),"O valor do Gasolina não foi informado!", Toast.LENGTH_LONG).show();
+            if(gasPrceStr.equals("")){
+                Toast.makeText(getApplicationContext(),"O valor da gasolina não foi informado",
+                        Toast.LENGTH_LONG).show();
                 return;
             }
 
             etanolPrice = Double.parseDouble(etanolPriceStr);
-            gasPrice = Double.parseDouble(gasPriceStr);
+            gasPrice = Double.parseDouble(gasPrceStr);
 
-            if (etanolPrice/gasPrice < 0.7){
+            if(etanolPrice/gasPrice < 0.7){
                 imageViewResult.setImageResource(R.drawable.ethanol);
-                tip = "Melhor usar Etanol";
-            } else {
+                tip = "Melhor usar etanol";
+            }else {
                 imageViewResult.setImageResource(R.drawable.gas);
-                tip = "Melhor usar Gasolina";
+                tip = "Melhor usar gasolina";
             }
 
             textViewResult.setText(tip);
-
             imageViewResult.setVisibility(ImageView.VISIBLE);
             textViewResult.setVisibility(TextView.VISIBLE);
             imageViewShare.setVisibility(ImageView.VISIBLE);
-
-
         });
 
+        imageViewShare.setOnClickListener(view -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
+            builder.setTitle("Preços de qual posto?");
+            LayoutInflater inflater = getLayoutInflater();
+            View layoutDialogView = inflater.inflate(R.layout.alert_dialog_gas_station_view, null);
+
+            builder.setView(layoutDialogView);
+
+            builder.setNegativeButton("Cancelar", null);
+
+            builder.setPositiveButton("Compartilhar", (dialogInterface, button ) ->{
+                EditText editText = layoutDialogView.findViewById(R.id.editTextAlertDialogId);
+                String posto = editText.getText().toString();
+
+                if(posto.equals("")){
+                    Toast toast = Toast.makeText(this, "Nome do posto nãpo pode ser vazio", Toast.LENGTH_SHORT);
+                    toast.show();
+                    return;
+                }
+
+                String message = String.format("Preço do posto '%s'. Gasolina: %.2f, Etanol: %.2f. %s. %.2fs"
+                        , posto, gasPrice, etanolPrice,tip, etanolPrice/gasPrice+100, "%");
+
+                Toast toast = Toast.makeText(this, message, Toast.LENGTH_LONG);
+                toast.show();
+
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, message);
+                sendIntent.setType("text/plain");
+
+                Intent shareIntent = Intent.createChooser(sendIntent, "");
+
+                startActivity(shareIntent);
+
+            });
+
+            builder.create().show();
+        });
     }
 
     @Override
@@ -88,6 +120,7 @@ public class MainActivity extends AppCompatActivity {
         imageViewResult.setVisibility(ImageView.INVISIBLE);
         textViewResult.setVisibility(TextView.INVISIBLE);
         imageViewShare.setVisibility(ImageView.INVISIBLE);
-
     }
+
+
 }
